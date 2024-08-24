84d0bd2a4e930bfe4f5a8d081f0b20974a7ce42d
Uladzimir <boba70325@gmail.com>
2024-08-24 18:28:27 +0300

    Update "src/s2-homeworks/hw01/HW1.tsx"

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -52,7 +52,7 @@ export const friendMessage0: MessageType = {
         name: 'Friend Name', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
+        text: 'сообщение для тренировки css', // можно менять
         time: '22:00', // можно менять
     },
 }
